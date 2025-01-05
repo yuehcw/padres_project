e66c90236337b0c85130c47a865c53e470afecf1
Yueh Chun Wu <yuehcw@uci.edu>
2025-01-05 15:16:09 -0800

    chore: migrate project from CRA to Vite

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,49 @@
+import { Link, useLocation } from 'react-router-dom';
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
+const Nav = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link
+            to={link.path}
+            key={index}
+            className={`${
+              link.path === pathname && "text-accent border-b-2 border-accent"
+            } capitalize font-medium hover:text-accent transition-all`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Nav;
--- frontend/src/services/playerbioService.js
+++ frontend/src/services/playerbioService.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
+const API_URL = import.meta.env.VITE_PROXY_URL || "http://127.0.0.1:5000";
 
 export const fetchPlayers = async () => {
   try {
